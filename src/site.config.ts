df6a0ff7f94b5978cc810186fa335bf2ce4cbb32
yuangwei <yuang.wya@gmail.com>
2023-11-16 14:36:34 +0800

    feat: change some blog style

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,17 +1,11 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
-	// Meta property used as the default description meta property
+	author: "Yuang Wei",
+	title: "Mind Power",
 	description: "An opinionated starter theme for Astro",
-	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
-	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-GB",
 		options: {
@@ -20,7 +14,6 @@ export const siteConfig: SiteConfig = {
 			year: "numeric",
 		},
 	},
-	// Include view-transitions: https://docs.astro.build/en/guides/view-transitions/
 	includeViewTransitions: false,
 	webmentions: {
 		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
@@ -34,11 +27,40 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		path: "/",
 	},
 	{
+		title: "Articles",
+		path: "/posts/",
+	},
+	{
 		title: "About",
 		path: "/about/",
 	},
-	{
-		title: "Blog",
-		path: "/posts/",
-	},
 ];
+
+export const socialLinks: Array<{
+	name: string;
+	friendlyName: string;
+	link: string;
+	isWebmention?: boolean;
+}> = [
+		{
+			name: "mdi:github",
+			friendlyName: "Github",
+			link: "https://github.com/yuangwei",
+		},
+		{
+			name: "mdi:linkedin",
+			friendlyName: "LinkedIn",
+			link: "https://www.linkedin.com/in/yuangwei/",
+		},
+		{
+			name: "mdi:twitter",
+			friendlyName: "Twitter",
+			link: "https://twitter.com/yuangwya/",
+		},
+		{
+			name: "mdi:email",
+			friendlyName: "email",
+			link: "mailto:yuang.wya@gmail.com",
+			isWebmention: true,
+		},
+	];
